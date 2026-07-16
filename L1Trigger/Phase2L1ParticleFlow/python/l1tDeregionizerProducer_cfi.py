a7ab800a7383396884d1afbfa026bce702c73794
JunwonTomOh <junwon.oh@cern.ch>
2026-07-16 18:30:36 +0200

    Temporarily changed the deregionzier into tm6

--- L1Trigger/Phase2L1ParticleFlow/python/l1tDeregionizerProducer_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1tDeregionizerProducer_cfi.py
@@ -1,54 +1,66 @@
 import FWCore.ParameterSet.Config as cms
 
 # Specifications for Correlator Layer 1 output mapping onto links
-barrelConfig = cms.PSet(
+barrelConfig_ = cms.PSet(
     partition = cms.string("Barrel"),
     nLinksPuppi = cms.uint32(6),
     nPuppiPerRegion = cms.uint32(18),
     nOutputFramesPerBX = cms.uint32(9),
-    outputBoard = cms.int32(0),
-    outputRegions = cms.vuint32(*range(54)),
-    tmuxFactor = cms.uint32(18)
+    tmuxFactor = cms.uint32(6),
 )
 
-hgcalConfig = cms.PSet(
+barrelPhiConfigs = [
+    barrelConfig_.clone(
+        outputRegions = cms.vuint32(*[3*ip+9*ie+i for ie in range(6) for i in range(3) ]),
+        outputBoard = cms.int32(ip),
+    ) for ip in range(3)
+]
+
+barrelPhiConfigs[2].outputBoard = 4
+
+hgcalConfig_ = cms.PSet(
     partition = cms.string("HGCal"),
-    nLinksPuppi = cms.uint32(2),
+    nLinksPuppi = cms.uint32(3),
     nPuppiPerRegion = cms.uint32(18),
     nOutputFramesPerBX = cms.uint32(9),
-    outputRegions = cms.vuint32(*range(54,72)),
-    outputBoard = cms.int32(1),
-    tmuxFactor = cms.uint32(18)
+    outputRegions = cms.vuint32(*[54 + i+9 for i in range(9)]),
+    outputBoard = cms.int32(3),
+    tmuxFactor = cms.uint32(6),
+)
+
+hgcalPosConfig = hgcalConfig_.clone(
+    outputBoard = 3
+)
+hgcalNegConfig = hgcalConfig_.clone(
+    outputRegions = [54 + i for i in range(9)],
+    outputBoard = 2
 )
 
 hgcalNoTKConfig = cms.PSet(
     partition = cms.string("HGCalNoTk"),
-    nLinksPuppi = cms.uint32(2),
+    nLinksPuppi = cms.uint32(4),
     nPuppiPerRegion = cms.uint32(12),
     nOutputFramesPerBX = cms.uint32(9),
     outputRegions = cms.vuint32(*range(72,72+18)),
-    outputBoard = cms.int32(2),
-    tmuxFactor = cms.uint32(18)
+    outputBoard = cms.int32(5),
+    tmuxFactor = cms.uint32(6),
 )
 
-# HF configuration is best estimate at the moment
-hfConfig = cms.PSet(
+hfConfig_ = cms.PSet(
     partition = cms.string("HF"),
-    nLinksPuppi = cms.uint32(1),
-    nPuppiPerRegion = cms.uint32(6),
+    nLinksPuppi = cms.uint32(3),
+    nPuppiPerRegion = cms.uint32(18),
     nOutputFramesPerBX = cms.uint32(9),
-    tmuxFactor = cms.uint32(6)
+    tmuxFactor = cms.uint32(6),
 )
-
 hfConfigs = [
-    hfConfig.clone(
+    hfConfig_.clone(
         outputRegions = cms.vuint32(*[90+9*ie+i for i in range(9)]),
-        outputBoard = cms.int32(3 + ie),
+        outputBoard = cms.int32(7 - ie),
     ) for ie in range(2)
 ]
 
-linkConfigs = cms.VPSet(barrelConfig, hgcalConfig, hgcalNoTKConfig, *hfConfigs)
-
+linkConfigs = cms.VPSet(*barrelPhiConfigs, hgcalPosConfig, hgcalNegConfig, hgcalNoTKConfig, *hfConfigs)
 
 l1tDeregionizerProducer = cms.EDProducer("DeregionizerProducer",
                            RegionalPuppiCands  = cms.InputTag("l1tLayer1","PuppiRegional"),
